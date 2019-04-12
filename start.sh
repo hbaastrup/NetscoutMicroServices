04d838549e16198412536ca130bda306503f05cf
Baastrup, Henrik <Henrik.Baastrup@netscout.com>
2019-04-12 17:34:29 +0200

    Changed the scripts to reflext the new orchestrating

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-java -cp MicroServices.1.0.0.jar playground.micro.web.tac.WebTAC &
-sleep 2
-java -cp MicroServices.1.0.0.jar playground.micro.web.subscriber.WebSubscriber &
-# slowdown response sometimes
-#java -cp MicroServices.1.0.0.jar playground.micro.web.subscriber.WebSubscriber -s &
-sleep 2
-java -cp MicroServices.1.0.0.jar playground.micro.web.cdr.database.WebCdr &
-sleep 2
-java -cp MicroServices.1.0.0.jar playground.micro.producers.CdrProducer &
-sleep 2
-java -cp MicroServices.1.0.0.jar playground.micro.monitor.Monitor &
+java -cp MicroServices.1.0.0.jar &
+#java -cp MicroServices.1.0.0.jar playground.micro.web.tac.WebTAC &
+#sleep 2
+#java -cp MicroServices.1.0.0.jar playground.micro.web.subscriber.WebSubscriber &
+## slowdown response sometimes
+##java -cp MicroServices.1.0.0.jar playground.micro.web.subscriber.WebSubscriber -s &
+#sleep 2
+#java -cp MicroServices.1.0.0.jar playground.micro.web.cdr.database.WebCdr &
+#sleep 2
+#java -cp MicroServices.1.0.0.jar playground.micro.producers.CdrProducer &
+#sleep 2
+#java -cp MicroServices.1.0.0.jar playground.micro.monitor.Monitor &
 echo "DONE!"
 
--- stop.sh
+++ stop.sh
@@ -2,6 +2,7 @@
 
 pids=`ps -ef | grep MicroServices.1.0.0.jar | awk '{print $2}'`
 #pids=`ps -ef | grep MicroServices- | awk '{print $2}'`
-kill -9 $pids
+kill -2 $pids
+#kill -9 $pids
 
 echo "DONE!"
